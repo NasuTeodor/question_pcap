3e98d0e75af46686e5c5e557b12aff9e9bcf214d
NasuTeodor <enderreal1@gmail.com>
2023-08-17 19:33:54 +0000

    packet capture si server

--- Monitor.js
+++ Monitor.js
@@ -1,4 +1,5 @@
 const pcap = require('pcap');
+const ws = require('ws')
 
 const device_name = 'wlan0'
 const options = {
@@ -7,15 +8,22 @@ const options = {
 }
 
 const pcap_session = pcap.createSession(device_name, options);
+const wss = new ws.WebSocketServer({ port: 3005 })
 
-pcap.warningHandler = function(warn) {
+console.log("pcap capture and server started")
+
+pcap.warningHandler = function (warn) {
     console.log(warn)
 }
 
-pcap_session.on('packet', (raw_packet)=>{
+pcap_session.on('packet', (raw_packet) => {
     // console.log(raw_packet);
     let packet = pcap.decode.packet(raw_packet);
-    console.log(packet)
+    // console.log(packet)
+    wss.on('connection', ()=>{
+        console.log('frontend connected')
+    })
+    wss.emit('message', 'got packet')
     // throw new Error("aici gata")
 })
 
