8c778f8cfb039c34ddc5336613436fb8c61d9a99
NasuTeodor <enderreal1@gmail.com>
2023-08-21 22:05:15 +0000

    idk nothing

--- Monitor.js
+++ Monitor.js
@@ -1,7 +1,7 @@
 const pcap = require('pcap');
 const ws = require('ws')
 
-const device_name = 'wlan0'
+const device_name = 'wlan0mon'
 const options = {
     promiscuous: true,
     monitor: false,
@@ -10,21 +10,31 @@ const options = {
 const pcap_session = pcap.createSession(device_name, options);
 const wss = new ws.WebSocketServer({ port: 3005 })
 
-wss.on('connection', ()=>{
+wss.on('connection', () => {
     console.log('Client connected!')
 })
 
 console.log("pcap capture and server started")
 
 pcap.warningHandler = function (warn) {
+    console.log("[PCAP WARN NEXT LINE]:")
     console.log(warn)
 }
 
 pcap_session.on('packet', (raw_packet) => {
-    let packet = pcap.decode.packet(raw_packet);
-    wss.clients.forEach((ws)=>{
-        ws.send(JSON.stringify(packet))
-    })    
+    console.log(raw_packet)
+    // #### TO DO NOW
+    // ADD CUSTOM PACKET PARSER FOR MONITOR MODE
+    // LOOKUP PCAP IN TCPDUMP
+    try {
+        let packet = pcap.decode.packet(raw_packet);
+        wss.clients.forEach((ws) => {
+            ws.send(JSON.stringify(packet))
+        })
+    } catch (e) {
+        wss.clients.forEach((ws) => { ws.send(JSON.stringify(e)) })
+        wss.clients.forEach((ws) => { ws.send(raw_packet) })
+    }
 })
 
 // TODO: ADD PACKET INJECTION
